7934054db9b061152c89e43680ca1eb3735f42b0
Prasad Ch <naidu.ch260@gmail.com>
2025-10-29 01:08:37 +0530

    adding script

--- JavaProject/src/practice/EvenNumbers.java
+++ JavaProject/src/practice/EvenNumbers.java
@@ -3,16 +3,13 @@ package Practice;
 public class EvenNumbers {
 
 	public static void main(String[] args) {
-		int[] i = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-		for (int num : i) {
-			if (num % 2 == 0) {
-				System.out.println("Even number is: " + num);
-			} else {
-				System.out.println("Odd number is: " + num);
+		int[] i= {1,2,3,4,5,6,7,8,9,10};
+		
+		for(int num : i) {
+			if(num % 2 == 0) {
+				System.out.println("Even numbers are: " + num);
 			}
 		}
-
 	}
 
 }
--- JavaProject/src/practice/NameReverseOrder.java
+++ JavaProject/src/practice/NameReverseOrder.java
@@ -4,13 +4,13 @@ public class NameReverseOrder {
 
 	public static void main(String[] args) {
 		String name = "Prasad";
-		char[] c = name.toCharArray();
+		char[] c =name.toCharArray();
 		
-		String reversName = "";
+		String reversedName="";
 		for(int i=c.length-1; i>=0; i--) {
-			reversName += c[i];
+			reversedName+=c[i];
 		}
-		System.out.println("reversed name is: " + reversName);
+		System.out.println(reversedName);
 	}
 
 }
