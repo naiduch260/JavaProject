be007aaedfbde297f7a196104dfe34f097598867
Prasad Ch <naidu.ch260@gmail.com>
2025-10-29 01:13:35 +0530

    adding script

---
+++ JavaProject/src/practice/lowerCase.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class lowerCase {
+
+	public static void main(String[] args) {
+		String name="PRASAD";
+		String lower=name.toLowerCase();
+		
+		System.out.println(lower);
+	}
+
+}
