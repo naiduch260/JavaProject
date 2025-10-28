0350b30b5f29c202bdd8ca4caca671096fb65511
Prasad Ch <naidu.ch260@gmail.com>
2025-10-29 02:45:55 +0530

    adding

--- JavaProject/src/practice/DupilcateNumbers.java
+++ JavaProject/src/practice/DupilcateNumbers.java
@@ -4,10 +4,8 @@ public class DupilcateNumbers {
 
 	public static void main(String[] args) {
 		int[] number= {1,2,3,4,1,3,2,3,2,3,4,5,6,7,8,5,7,4,6,3,2,8,9,7,0,8,0,6,5,4};
-		
 		int num=0;
 		int count=0;
-		
 		for(int value : number) {
 			if(value==num) {
 				count++;
